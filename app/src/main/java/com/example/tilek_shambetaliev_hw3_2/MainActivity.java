946af2866e3390630c313a17de6c647f4061d050
Tshambetaliev <tshambetaliev@gmail.com>
2023-02-12 15:47:45 +0600

    return

--- app/src/main/java/com/example/tilek_shambetaliev_hw3_2/MainActivity.java
+++ app/src/main/java/com/example/tilek_shambetaliev_hw3_2/MainActivity.java
@@ -14,9 +14,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-if (savedInstanceState == null) {
+
     getSupportFragmentManager().beginTransaction()
             .add(R.id.conteiner, new FirstFragment()).commit();
-}
+
     }
 }
